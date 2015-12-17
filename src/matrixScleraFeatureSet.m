367efcd405d43ff6c637574e09c1eb6e7380ab37
bdsword <thetruebloodsword@gmail.com>
2015-12-17 22:00:32 +0800

    Adjust border cell value

--- src/matrixScleraFeatureSet.m
+++ src/matrixScleraFeatureSet.m
@@ -10,24 +10,32 @@ featureSet = [];
 I2 = I.^2;
 
 if imageChannel == 1
-   u0_I  = I;
+    u0_I  = I;
    u0_I2 = I2;
    sigma0 = sqrt( u0_I2 - u0_I.^2 );
    
    u3_I  = colfilt(I, [7 7], 'sliding', @matrixAverageIntensity);
+   u3_I = adjustImage(u3_I, 3);
    u3_I2 = colfilt(I2, [7 7], 'sliding', @matrixAverageIntensity);
+   u3_I2 = adjustImage(u3_I2, 3);
    sigma3 = sqrt( u3_I2 - u3_I.^2 );
    
    u5_I  = colfilt(I, [11 11], 'sliding', @matrixAverageIntensity);
+   u5_I = adjustImage(u5_I, 5);
    u5_I2 = colfilt(I2, [11 11], 'sliding', @matrixAverageIntensity);
+   u5_I2 = adjustImage(u5_I2, 5);
    sigma5 = sqrt( u5_I2 - u5_I.^2 );
    
    u7_I  = colfilt(I, [15 15], 'sliding', @matrixAverageIntensity);
+   u7_I = adjustImage(u7_I, 7);
    u7_I2 = colfilt(I2, [15 15], 'sliding', @matrixAverageIntensity);
+   u7_I2 = adjustImage(u7_I2, 7);
    sigma7 = sqrt( u7_I2 - u7_I.^2 );
    
    u9_I  = colfilt(I, [19 19], 'sliding', @matrixAverageIntensity);
+   u9_I = adjustImage(u9_I, 9);
    u9_I2 = colfilt(I2, [19 19], 'sliding', @matrixAverageIntensity);
+   u9_I2 = adjustImage(u9_I2, 9);
    sigma9 = sqrt( u9_I2 - u9_I.^2 );
 else
     disp('Not implements yet');
